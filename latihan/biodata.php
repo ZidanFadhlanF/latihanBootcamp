20d3c44c35c6c49a4b0e82c67adaeab18331d8fe
ZidanFadhlanF <zidan161104@gmail.com>
2023-09-29 08:18:40 +0700

    latihanBootcamp

---
+++ latihan/biodata.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <style>
+        table, tr, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+    </style>
+
+</head>
+<body>
+    <?php echo "BIODATA";
+    ?>
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>Zidan Fadhlan F</td>
+        </tr>
+        <tr>
+            <td>NPM</td>
+            <td>227006071</td>
+        </tr>
+        <tr>
+            <td>Hobi</td>
+            <td>Badminton</td>
+        </tr>
+       
+
+    </table>
+
+</body>
+</html>
